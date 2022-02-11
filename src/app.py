23d52f1130631b052c050a684f1224872ead2068
Dominic Martinez <64941386+Villix-Main@users.noreply.github.com>
2022-02-10 23:16:54 -0500

    Added comments to server code

--- src/app.py
+++ src/app.py
@@ -2,17 +2,17 @@
 # link to FLASK framework - https://github.com/pallets/flask
 # link to SQlAlchemy - https://www.sqlalchemy.org/
 from flask import Flask, render_template, url_for, request, redirect
-from flask.wrappers import Request
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import datetime
-import json
+
 
 # Creates an instance 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///appdictionary.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contactdatabase.db'
 db = SQLAlchemy(app)
 
 
+# The Contact Database Table Model
 class Contact(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256), nullable=False)
@@ -20,35 +20,48 @@ class Contact(db.Model):
     email = db.Column(db.String(2048), nullable=False, default="")
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
-    def __repr__(self):
-        return '<Contact %r>' % self.id
 
 
-def SearchContacts(contactList, searchItem):
+# Searches the contact database and tries to see if the searchItem
+# Matches with any name of an contact 
+def SearchContacts(searchItem):
+    # Get all records in the Contact Database Table
+    contacts = Contact.query.all()
+    # This is the list that will contain all the possible search results and will be returned
     search_result = list()
 
-    for item in contactList:
-        if item.name.lower() == searchItem or searchItem in item.name.lower():
+    # Iterates through all items in the contact database and checks if any
+    # Contact name matches with searchItem and also if searchItem is a substring
+    # of a name, if any checks are true, that contact is added to search result
+    for item in contacts:
+        if searchItem in item.name.lower():
             search_result.append(item)
+
     return search_result
 
 
 @app.route('/')
 def Index():
+    # Checks if the route included a search query
     if (request.args.get("search")):
+        # Get search query from the GET request
         search = request.args.get("search").lower()
-        contacts = Contact.query.all()
 
-        result = SearchContacts(contacts, search)
-
-        return render_template('search_result.html', contacts=result)
+        # Check if any of contacts name matches the users search by
+        # Calling SearchContact
+        result = SearchContacts(search)
+        
+        # Render the search_result HTMl and passing the search result as contacts data
+        return render_template('search_result.html', search_results=result)
     else:
         return render_template('index.html')
 
 @app.route('/viewall')
 def viewAll():
+    # Get all records in Contact Database Table
     contacts = Contact.query.all()
-
+    
+    # Renders view_all HTML with contacts data being passed
     return render_template('view_all.html', contacts=contacts)
 
 
@@ -65,27 +78,34 @@ def addContact():
         new_contact = Contact(name=contact_name, phonenumber=contact_phonenumber, email=contact_email)
 
         try:
-            # Add the new contact to the contact dictionary 
+            # Add the new contact to the contact database session
             db.session.add(new_contact)
             
-            # Commit 
+            # Commit the current transaction to the database
             db.session.commit()
+            # Redirect user to /viewall URL
             return redirect('/viewall')
         except:
+            # Display error if adding or commiting to database failed
             return 'There was a issue when creating and adding the term'
     else:
+        # If request was a GET, render the add contact HTML to allow user to create a contact
         return render_template('add_contact.html')
 
 
 @app.route('/deletecontact/<int:id>')
 def delete(id):
+    # Get the contact to delete using it's ID
     delete_contact = Contact.query.get_or_404(id)
 
     try:
+        # Attempt to delete the contact by passing in its object form
         db.session.delete(delete_contact)
+        # Commit the current transaction to the database
         db.session.commit()
         return redirect('/viewall')
     except:
+        # Display error if adding or commiting to database failed
         return 'We ran into an issue while removing the contact, please try again'
 
 
